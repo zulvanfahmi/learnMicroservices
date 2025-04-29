607891d18d7ee1abda181a70073fce052a1f5dc1
Muhamad Fahmi <zulvan.fahmi@ibm.com>
2025-04-29 15:43:16 +0700

    testbean done

--- src/main/java/com/restWebService/project/restfull_web_services/controllers/TestBean.java
+++ src/main/java/com/restWebService/project/restfull_web_services/controllers/TestBean.java
@@ -1,9 +1,21 @@
 package com.restWebService.project.restfull_web_services.controllers;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TestBean {
 
     private String message;
 
-    
+    public TestBean(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Hello world test bean " + message ;
+    }
 
 }
