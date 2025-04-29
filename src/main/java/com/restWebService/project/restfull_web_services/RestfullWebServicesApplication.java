d55cd86099d7ac1469a41f0d5c55f8e9b5825800
Muhamad Fahmi <zulvan.fahmi@ibm.com>
2025-04-29 14:39:59 +0700

    inital project

---
+++ src/main/java/com/restWebService/project/restfull_web_services/RestfullWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.restWebService.project.restfull_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restWebService/project/restfull_web_services/RestfullWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restWebService.project.restfull_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
